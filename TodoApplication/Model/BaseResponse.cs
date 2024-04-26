030502a202759758271f36af2cbaa2d3acf89a9d
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-04-26 22:19:56 +0530

    Added BaseResponse in model

---
+++ TodoApplication/Model/BaseResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace TodoApplicationWebAPI.Model
+{
+    public class BaseResponse<T>
+    {
+        public string? ResponseCode { get; set; }
+
+        public string? ResponseMessage { get; set; }
+
+        public string? ResultMessage { get; set; }
+        public T? ResultsetData { get; set; }
+
+    }
+    //public class BaseResponse
+    //{
+    //    public string? ResponseCode { get; set; }
+
+    //    public string? ResponseMessage { get; set; }
+
+    //    public string? ResultMessage { get; set; }
+    //}
+}
